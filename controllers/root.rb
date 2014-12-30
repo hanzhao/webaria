d81fcda754b3c72865e6e966da4c1d4e4c74b2e7
Magica <i@0u0.moe>
2014-12-30 13:43:50 +0800

    Add uri check

--- controllers/root.rb
+++ controllers/root.rb
@@ -13,17 +13,23 @@ post '/' do
   status = -1
   gid ||= nil
   msg ||= nil
-  begin
-    gid = ARIA2.download(uri, "#{SINATRA_ROOT}/public/downloads/#{filename}")
-    status = 0
-    msg = 'Task has been added successfully.'
-    Task.create(uri:      uri,
-                filename: filename,
-                gid:      gid,
-                status:   status)
-  rescue
+  task = Task.find_by_uri(uri)
+  if task
+    msg = 'Task has already taken before.'
     status = -1
-    msg = 'Invalid resource url.'
+  else
+    begin
+      gid = ARIA2.download(uri, "#{SINATRA_ROOT}/public/downloads/#{filename}")
+      status = 0
+      msg = 'Task has been added successfully.'
+      Task.create(uri:      uri,
+                  filename: filename,
+                  gid:      gid,
+                  status:   status)
+    rescue
+      status = -1
+      msg = 'Invalid resource url.'
+    end
   end
   erb :index, locals: { msg: msg }
 end
